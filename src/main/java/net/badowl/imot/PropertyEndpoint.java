b30152cf00e97d67f857d496501550607da985b7
nyxz <master033@gmail.com>
2019-11-10 16:09:52 +0200

    endpoint:async: Add error handling for the async methods execution.

--- src/main/java/net/badowl/imot/PropertyEndpoint.java
+++ src/main/java/net/badowl/imot/PropertyEndpoint.java
@@ -4,7 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PropertyEndpoint {
@@ -16,18 +19,23 @@ public class PropertyEndpoint {
     @PostMapping(path = "/scrape")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void scrape() throws Exception {
-        scraper.scrapeAll();
+        scraper.scrapeAll().exceptionally(this::handleAsyncScrapeError);
     }
 
     @PostMapping(path = "/scrape-notify")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void scrapeAndNotify() throws Exception {
-        scraper.scrapeAndNotify();
+        scraper.scrapeAndNotify().exceptionally(this::handleAsyncScrapeError);
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public void errorHandler(Exception ex) {
         LOG.error("Global exception handler", ex);
+    }
+
+    private Void handleAsyncScrapeError(final Throwable th) {
+        LOG.error("Async scraper error: ", th);
+        return null;
     }
 }
