4b72ef7474309cba0d6941f5754d71018f6db372
nyxz <master033@gmail.com>
2020-05-25 15:45:58 +0300

    email-data: Format the email price.

--- src/main/java/net/badowl/imot/PropertyEmailData.java
+++ src/main/java/net/badowl/imot/PropertyEmailData.java
@@ -5,11 +5,23 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Optional;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class PropertyEmailData {
+    private static final DecimalFormat DECIMAL_FORMAT;
+
+    static {
+        final DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+        symbols.setGroupingSeparator(' ');
+        DECIMAL_FORMAT = new DecimalFormat("###,###.##", symbols);
+    }
+
     private String type;
     private String area;
     private String address;
@@ -19,4 +31,8 @@ public class PropertyEmailData {
     private Integer buildYear;
     private Integer floor;
     private Integer totalFloors;
+
+    public String getPriceFmt() {
+        return Optional.ofNullable(price).map(DECIMAL_FORMAT::format).orElse(null);
+    }
 }
